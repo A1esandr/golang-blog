25a4e06e2e4c03d5a381883530fe28f98ef8430a
A1esandr <ravulmirt@gmail.com>
2021-01-01 17:34:52 +0500

    Added example with channel and atomic

---
+++ src/goroutines/sync-channels-atomic-for.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+// Example of syncing with channel and atomic and for
+
+func main() {
+	var counter int64
+	increment := func(c *int64, ch chan int) {
+		atomic.AddInt64(c, 1)
+		ch <- 1
+	}
+	ch := make(chan int, 1)
+	var i int64
+	for i = 0; i < 20; i++ {
+		go increment(&counter, ch)
+	}
+	for ; i > 0; i-- {
+		<-ch
+	}
+
+	for i = 0; i < counter; i++ {
+		go func(i int64) {
+			fmt.Printf("Hello %d\n", i)
+			ch <- 1
+		}(i)
+	}
+	for i = 0; i < counter; i++ {
+		<-ch
+	}
+	fmt.Println(counter)
+}
