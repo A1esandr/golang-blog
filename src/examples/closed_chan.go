4081d9c0b9007c626a6a01ce663ca2f90a70fc5f
A1esandr <a1exswan001@gmail.com>
2021-04-01 00:01:46 +0500

    Added example of closed buffered channel wait

--- src/examples/closed_chan.go
+++ src/examples/closed_chan.go
@@ -4,6 +4,12 @@ import "fmt"
 
 // Receiving from channel will wait until channel have closed
 func main() {
+	not_buffered()
+	buffered()
+}
+
+func not_buffered() {
+	fmt.Println("not_buffered")
 	w := make(chan struct{})
 
 	go func() {
@@ -13,5 +19,19 @@ func main() {
 		close(w)
 	}()
 	<-w
-	fmt.Println("Finish")
+	fmt.Println("Finish not_buffered")
+}
+
+func buffered() {
+	fmt.Println("buffered")
+	w := make(chan struct{}, 10)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println(i)
+		}
+		close(w)
+	}()
+	<-w
+	fmt.Println("Finish buffered")
 }
